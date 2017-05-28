5f2a88c97e3583f20a5b6a70ca6c40f8c364dcf4
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-05-28 15:22:49 +0200

    Created first files

---
+++ src/js/CanvasInit.js
@@ -0,0 +1,24 @@
+/**
+ * Created by mehdy on 28/05/17.
+ */
+let oApp = {
+    "canvas": null,
+    "context": null,
+    "width": null,
+    "height": null,
+};
+
+const setup = function() {
+    oApp.canvas = document.querySelector( "#game" );
+    if ( !oApp.canvas.getContext ) {
+        console.error( "Your browser doesn't support canvas!" );
+        return;
+    }
+    oApp.context = oApp.canvas.getContext( "2d" );
+    oApp.width = oApp.canvas.width;
+    oApp.height = oApp.canvas.height;
+
+    new Flappy( oApp );
+};
+
+setup();
