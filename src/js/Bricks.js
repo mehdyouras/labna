b92bc6ffb95b2edb343b3c41effc2a4719587f14
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-05-29 20:01:26 +0200

    Fixed bricks

--- src/js/Bricks.js
+++ src/js/Bricks.js
@@ -9,34 +9,39 @@ class Bricks {
         this.bricksOrigin = {
             'x' : 50,
             'y' : 80
-        }
+        };
         this.brickWidth = (width-50)/10;
         this.brickHeight = 30;
     }
     generateBricksArray() {
-        while (i < 10) {
+        let bricks = [];
+        for (i = 0; i < 10; i++) {
             let currentBrick;
             currentBrick = {
                 'x' : (this.bricksOrigin.x)+(i*this.brickWidth),
                 'y' : this.bricksOrigin.y,
-            }
-            i++;
+                "health" : 3
+            };
+            bricks += currentBrick;
         }
-        while (i <= 8) {
+        for (i = 0; i < 8; i++) {
             let currentBrick;
             currentBrick = {
-                'x' : (this.bricksOrigin.x)+(i*this.brickWidth),
-                'y' : this.bricksOrigin.y,
-            }
-            i++;
+                'x' : (this.bricksOrigin.x)+(i*this.brickWidth)+(this.brickWidth),
+                'y' : (this.bricksOrigin.y)+this.brickHeight,
+                "health" : 2
+            };
+            bricks += currentBrick;
         }
-        while (i <= 6) {
+        for (i = 0; i < 6; i++) {
             let currentBrick;
             currentBrick = {
-                'x' : (this.bricksOrigin.x)+(i*this.brickWidth),
-                'y' : this.bricksOrigin.y,
-            }
-            i++;
+                'x' : (this.bricksOrigin.x)+(i*this.brickWidth)+(2*this.brickWidth),
+                'y' : (this.bricksOrigin.y)+(2*this.brickHeight),
+                "health" : 1
+            };
+            bricks += currentBrick;
         }
+        return bricks;
     }
 }
