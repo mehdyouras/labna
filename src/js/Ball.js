6a8860135852dbb51d33cd00740758fc5cf36195
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-05-30 12:37:07 +0200

    Added changeDirection function

--- src/js/Ball.js
+++ src/js/Ball.js
@@ -9,18 +9,18 @@ class Ball {
 
         this.isLaunched = false;
         this.radius = 10;
-        this.ballCenter = {
+        this.center = {
             x: this.width/2,
             y: this.height-65,
         }
         this.speed = 0;
     }
     draw() {
-        let {context, ballCenter, radius, speed} = this;
-        ballCenter.y += speed;
+        let {context, center, radius, speed} = this;
+        center.y += speed;
 
         context.beginPath();
-        context.arc(ballCenter.x,ballCenter.y,radius,0,2*Math.PI);
+        context.arc(center.x,center.y,radius,0,2*Math.PI);
         context.fill();
         context.closePath();
     }
@@ -29,5 +29,13 @@ class Ball {
             this.speed = -4;
         }
         this.isLaunched = true;
+    }
+    changeDirection() {
+        if(this.speed === -4) {
+            this.speed = 4;
+        } else {
+            this.speed = -4;
+        }
+
     }
 }
