3bd66c5c6879480ec512cb10ff2805e9a02b16e5
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-05-30 10:34:37 +0200

    Finished platform constructor

--- src/js/Platform.js
+++ src/js/Platform.js
@@ -1,3 +1,25 @@
 /**
  * Created by mehdy on 28/05/17.
  */
+class Platform {
+    constructor({width, height, context}) {
+        this.width = width;
+        this.height = height;
+        this.context = context;
+
+
+        this.platformWidth = 70;
+        this.platformHeight = 15;
+        this.platformSpeed = 3;
+        this.platformColor = "#ab94ff";
+
+        this.platformOrigin = {
+            x: (this.width/2)-(this.platformWidth/2),
+            y: 30,
+        }
+    }
+    draw() {
+        let {context, platformWidth, platformHeight, platformOrigin, platformColor} = this;
+
+    }
+}
