793f4338a4981f1b836c3e9fd0d41e1770970bee
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-05-30 10:34:06 +0200

    Finished drawing Bricks

--- src/js/Bricks.js
+++ src/js/Bricks.js
@@ -2,20 +2,22 @@
  * Created by mehdy on 28/05/17.
  */
 class Bricks {
-    constructor(width, height, context) {
+    constructor({width, height, context}) {
         this.width = width;
         this.height = height;
         this.context = context;
 
         this.bricksOrigin = {
-            'x' : 50,
-            'y' : 80
+            'x' : 25,
+            'y' : 60
         };
-        this.brickWidth = (width-50)/10;
+        this.brickWidth = (this.width-50)/10;
         this.brickHeight = 30;
     }
     generateBricksArray() {
-        let bricks = [];
+        this.bricks = [];
+        let i,
+            bricks = [];
         for (i = 0; i < 10; i++) {
             let currentBrick;
             currentBrick = {
@@ -23,7 +25,7 @@ class Bricks {
                 'y' : this.bricksOrigin.y,
                 "health" : 3
             };
-            bricks += currentBrick;
+            bricks.push(currentBrick);
         }
         for (i = 0; i < 8; i++) {
             let currentBrick;
@@ -32,7 +34,7 @@ class Bricks {
                 'y' : (this.bricksOrigin.y)+this.brickHeight,
                 "health" : 2
             };
-            bricks += currentBrick;
+            bricks.push(currentBrick);
         }
         for (i = 0; i < 6; i++) {
             let currentBrick;
@@ -41,13 +43,26 @@ class Bricks {
                 'y' : (this.bricksOrigin.y)+(2*this.brickHeight),
                 "health" : 1
             };
-            bricks += currentBrick;
+            bricks.push(currentBrick);
         }
+
         this.bricks = bricks;
     }
     draw() {
+        let {context, brickWidth, brickHeight} = this;
+        let healthColor = [ "#b07b4b", "#b0ae4b", "#88b04b"];
+
+
+        context.strokeStyle = "#000000";
+        context.lineWidth   = 1;
+
+
         this.bricks.forEach(function(brick) {
-            this.context.fillRect(brick.x, brick.y, this.brickWidth, this.brickHeight)
+            if(brick.health > 0) {
+                context.fillStyle=healthColor[brick.health-1];
+                context.fillRect(brick.x, brick.y, brickWidth, brickHeight);
+                context.strokeRect(brick.x,brick.y, brickWidth,brickHeight);
+            }
         });
     }
 }
