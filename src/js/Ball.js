598e36cb4625881ac43e28e33662ef84a6c7d467
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-05-30 12:06:18 +0200

    Handled ball launch

--- src/js/Ball.js
+++ src/js/Ball.js
@@ -1,3 +1,33 @@
 /**
  * Created by mehdy on 28/05/17.
  */
+class Ball {
+    constructor({width, height, context}) {
+        this.width = width;
+        this.height = height;
+        this.context = context;
+
+        this.isLaunched = false;
+        this.radius = 10;
+        this.ballCenter = {
+            x: this.width/2,
+            y: this.height-65,
+        }
+        this.speed = 0;
+    }
+    draw() {
+        let {context, ballCenter, radius, speed} = this;
+        ballCenter.y += speed;
+
+        context.beginPath();
+        context.arc(ballCenter.x,ballCenter.y,radius,0,2*Math.PI);
+        context.fill();
+        context.closePath();
+    }
+    handleAction() {
+        if(!this.isLaunched) {
+            this.speed = -4;
+        }
+        this.isLaunched = true;
+    }
+}
--- src/js/Labna.js
+++ src/js/Labna.js
@@ -10,6 +10,8 @@ class Labna {
         this.setup();
     }
     setup() {
+        document.addEventListener( "keydown", this.handleAction.bind( this ) );
+        document.addEventListener( "keydown", this.handleAction.bind( this ) );
         this.reset();
     }
     reset() {
@@ -17,6 +19,7 @@ class Labna {
         this.bricks.generateBricksArray();
 
         this.platform = new Platform(this);
+        this.ball = new Ball(this);
 
         this.draw();
 
@@ -27,5 +30,20 @@ class Labna {
 
         this.bricks.draw();
         this.platform.draw();
+        this.ball.draw();
+    }
+    handleAction(oEvent) {
+        if(oEvent.keyCode == 32) {
+            this.ball.handleAction();
+        }
+        if(!this.ball.isLaunched) {
+            return; // Doesn't allow platform movement if the ball is not launched
+        }
+        if( oEvent.keyCode == 37 ) {
+            this.platform.handleMovement("left")
+        }
+        if( oEvent.keyCode == 39 ) {
+            this.platform.handleMovement("right")
+        }
     }
 }
